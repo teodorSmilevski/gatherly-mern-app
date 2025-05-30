b6cadca2283a4716fd0b342b524150beb9163ab1
teodorSmilevski <teosmilevski@outlook.com>
2025-05-30 15:48:16 +0200

    Feat: backend implementation and mongo setup

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running.");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
